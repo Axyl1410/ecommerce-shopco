509f36131200898587f0f08d129edc856f90cd5c
Alex <truonggiang190689@gmail.com>
2025-10-26 00:24:53 +0700

    use proxy

--- frontend/src/lib/stripe.ts
+++ frontend/src/lib/stripe.ts
@@ -1,8 +1,9 @@
 import "server-only";
 
 import Stripe from "stripe";
-import { assertValue } from "./utils";
 
 export const stripe = new Stripe(
-  assertValue(process.env.STRIPE_SECRET_KEY, "Missing STRIPE_SECRET_KEY")
+  // todo: use later for production
+  // assertValue(process.env.STRIPE_SECRET_KEY, "Missing STRIPE_SECRET_KEY")
+  process.env.STRIPE_SECRET_KEY as string,
 );

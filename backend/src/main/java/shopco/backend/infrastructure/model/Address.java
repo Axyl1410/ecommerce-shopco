43553c5d41613564efb509aad59e4721433fddfb
Alex <truonggiang190689@gmail.com>
2025-10-27 13:32:29 +0700

    Enhance data models and update application properties
    - Added `updatedAt` timestamp to the `Address` entity for tracking modifications.
    - Introduced `IdClass` for composite keys in `CouponApplicableCategory`, `CouponApplicableProduct`, `CouponExcludedCategory`, and `CouponExcludedProduct` entities to improve data integrity.
    - Updated `ProductTag` entity to include `IdClass` for composite keys.
    - Changed `spring.jpa.hibernate.ddl-auto` setting to `none` in `application.properties` to prevent automatic schema updates.

--- backend/src/main/java/shopco/backend/infrastructure/model/Address.java
+++ backend/src/main/java/shopco/backend/infrastructure/model/Address.java
@@ -1,11 +1,20 @@
 package shopco.backend.infrastructure.model;
 
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "address")
@@ -51,4 +60,8 @@ public class Address {
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
+    
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
 }

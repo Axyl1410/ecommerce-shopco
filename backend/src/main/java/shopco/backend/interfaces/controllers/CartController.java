231515e325efc6467f3bf718679e152e2bd4e330
Alex <truonggiang190689@gmail.com>
2025-11-03 20:57:34 +0700

    refactor: Update CartController API endpoint path
    - Changed the request mapping for CartController from "/api/cart" to "/cart" to simplify the API structure.

--- backend/src/main/java/shopco/backend/interfaces/controllers/CartController.java
+++ backend/src/main/java/shopco/backend/interfaces/controllers/CartController.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/cart")
+@RequestMapping("/cart")
 public class CartController {
     
     private final ICartService cartService;
